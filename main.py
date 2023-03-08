8c350b136445cb195a80b5814dba2e0b1d4caa06
Csongor Pilinszki-Nagy <csongor.pilinszkinagy@gmail.com>
2023-03-08 11:16:20 +0000

    Comments added

--- main.py
+++ main.py
@@ -4,48 +4,71 @@ from progressbar import progressbar
 import pandas as pd
 
 def scraper() -> pd.DataFrame:
+    '''
+    Function for scraping the top 20 movie titles, ratings, number of
+    ratings and number of Oscars in a pandas DataFrame
+    '''
+    # Open the IMDB top 250 page
     url = 'https://www.imdb.com/chart/top/'
     with requests.get(url) as page:
         soup = BeautifulSoup(page.text, 'html.parser')
 
+        # Scrape titles from the titleColumn cells
         titles = [elem.text for elem in soup.select('td.titleColumn a')]
+        # Make sure that we get back all the 250 titles as strings
         assert len(titles) == 250
         assert all(type(title) == str for title in titles)
 
+        # Scrape ratings from the posterColumns cell, the data value is called 'ir'
         ratings = [float(elem.get('data-value')) for elem in soup.select('td.posterColumn span[name=ir]')]
+        # Make sure that we get back all the 250 ratings as floats
         assert len(ratings) == 250
         assert all(type(rating) == float for rating in ratings)
 
+        # Scrape number of ratings from the posterColumns cell, the data value is called 'nv'
         num_ratings = [int(elem.get('data-value')) for elem in soup.select('td.posterColumn span[name=nv]')]
+        # Make sure that we get back all 250 rating numbers as ints
         assert len(num_ratings) == 250
         assert all(type(num) == int for num in num_ratings)
 
+        # For the oscars the movies need to be scraped individually
+        # Get the hrefs for the movie pages from the titleColumns cell
         hrefs = [elem.get('href') for elem in soup.select('td.titleColumn a')]
+        # Make sure that we get back all 250 links as strings
         assert len(hrefs) == 250
         assert all(type(href) == str for href in hrefs)
 
+        # Complete the hrefs to point to award pages
         award_urls = [f'https://imdb.com{href}awards' for href in hrefs]
 
+        # Iterate over top 20 urls and get the oscar numbers
         num_oscars = []
         for url in progressbar(award_urls[:20]):
+            # Open movie awards page
             with requests.get(url) as awards_page:
                 soup = BeautifulSoup(awards_page.text, 'html.parser')
 
+                # Select award outcome cells
                 awards = soup.select('td.title_award_outcome')
-                oscar_winner = None
+                
+                # Find the oscar cell if it exists
+                oscar = None
                 for award in awards:
                     category = award.select_one('span.award_category')
-                    outcome = award.select_one('b')
-                    if category.text == 'Oscar' and outcome.text == 'Winner':
-                        oscar_winner = award.parent.parent
+                    if category.text == 'Oscar':
+                        oscar = award.parent.parent
 
-                if oscar_winner:
-                    num_oscars.append(len(oscar_winner.select('tr')))
+                # Append the number of oscars to the list
+                if oscar:
+                    num_oscars.append(len(oscar.select('tr')))
                 else:
                     num_oscars.append(0)
+
+        # Make sure that we get all 20 oscar numbers as ints      
         assert len(num_oscars) == 20
         assert all(type(num) == int for num in num_oscars)
 
+        # Assemble top 20 dataframe from the scraped data
         df = pd.DataFrame()
         df['title'] = titles[:20]
         df['rating'] = ratings[:20]
